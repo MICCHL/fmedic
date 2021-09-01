36893b4428214696e267cffb3c1e6c6e125eb738
Michał <michal.chlipala2@gmail.com>
2021-09-01 10:42:14 +0200

    modification VisitRepository

--- src/Repository/VisitRepository.php
+++ src/Repository/VisitRepository.php
@@ -2,8 +2,11 @@
 
 namespace App\Repository;
 
+use App\Entity\Doctor;
+use App\Entity\User;
 use App\Entity\Visit;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -18,4 +21,47 @@ class VisitRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Visit::class);
     }
+
+    /**
+     * @return Visit[] Returns an array of Visit objects
+     */
+    public function findBookedVisits(Doctor $doctor, \DateTimeImmutable $startAt, \DateTimeImmutable $endAt)
+    {
+        return $this->createQueryBuilder('v')
+            ->andWhere('v.doctor = :doctor')
+            ->setParameter('doctor', $doctor)
+            ->andWhere('v.date BETWEEN :startDate AND :endDate')
+            ->setParameter('startDate', $startAt)
+            ->setParameter('endDate', $endAt)
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findUserVisit(User  $user)
+    {
+        return $this->createQueryBuilder('v')
+            ->andWhere('v.patient = :patient')
+            ->setParameter('patient', $user)
+            ->orderBy('v.date')
+            ->orderBy('v.status')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findDoctorVisit(User  $user)
+    {
+        return $this->createQueryBuilder('v')
+            ->andWhere('v.status = :status')
+            ->setParameter('status', 0)
+            ->andWhere('v.date BETWEEN :startDate AND :endDate')
+            ->setParameter('startDate', date('Y-m-d 00:00:00'))
+            ->setParameter('endDate', date('Y-m-d 23:59:59'))
+            ->join('v.doctor', 'd')
+            ->andWhere('d.user = :user')
+            ->setParameter('user', $user)
+            ->orderBy('v.date')
+            ->getQuery()
+            ->getResult();
+    }
+
 }
