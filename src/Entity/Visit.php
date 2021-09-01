ad6f04df911b083cff7ccfa2bec6b58f80512bf5
Michał <michal.chlipala2@gmail.com>
2021-09-01 10:42:28 +0200

    modification Visit

--- src/Entity/Visit.php
+++ src/Entity/Visit.php
@@ -20,21 +20,108 @@ class Visit
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $title;
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $summary;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Doctor::class, inversedBy="visits")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $doctor;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="visits")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $patient;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getTitle(): ?string
+    public function getDescription(): ?string
     {
-        return $this->title;
+        return $this->description;
     }
 
-    public function setTitle(string $title): self
+    public function setDescription(string $description): self
     {
-        $this->title = $title;
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getSummary(): ?string
+    {
+        return $this->summary;
+    }
+
+    public function setSummary(?string $summary): self
+    {
+        $this->summary = $summary;
+
+        return $this;
+    }
+
+    public function getDoctor(): ?Doctor
+    {
+        return $this->doctor;
+    }
+
+    public function setDoctor(?Doctor $doctor): self
+    {
+        $this->doctor = $doctor;
+
+        return $this;
+    }
+
+    public function getPatient(): ?User
+    {
+        return $this->patient;
+    }
+
+    public function setPatient(?User $patient): self
+    {
+        $this->patient = $patient;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
 
         return $this;
     }
